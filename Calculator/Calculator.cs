43bb3ed498e4fe2913800ae7dcd2e065fe5aedb1
suryadi121 <suryadi.surya5@gmail.com>
2014-04-27 11:48:58 +0100

    method convert

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -40,6 +40,39 @@ namespace Calculator
                 }
             return cekTrue;
         }
+        public void convert(string inp, ArrayList arr)
+        {
+            if (validasi(input)) //cek validasi TRUE atau False
+            {
+                string tmp="";  //untuk tempat sementara bilangan
+                if (inp[0] != '(')
+                    tmp += inp[0];
+                else
+                    arr.Add(inp[0]);
+                for (int i = 1; i < inp.Length; i++) //for dari inp[1] bukan inp[0]
+                {
+                    if(inp[i]=='^'||inp[i]=='*'||inp[i]=='/'||inp[i]=='+'||inp[i]=='('||inp[i]==')')
+                    {
+                        arr.Add(Convert.ToDouble(tmp));
+                        tmp = "";
+                        arr.Add(inp[i]);
+                    }
+                    else if(inp[i]=='-')
+                    {
+                        if(inp[i-1]=='^'||inp[i-1]=='*'||inp[i-1]=='/'||inp[i-1]=='+')
+                            tmp+=inp[i];
+                        else
+                            arr.Add(inp[i]);
+                    }
+                    else
+                        tmp+=inp[i];
+                }
+                if (tmp != "")
+                    arr.Add(Convert.ToDouble(tmp));
+            }
+            else
+                Console.WriteLine("Error!!!...");
+        }
 
     }
 }
