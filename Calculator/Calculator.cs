e89af823344c9777ba0b4793c75088ef9c29ef4b
suryadi121 <suryadi.surya5@gmail.com>
2014-04-27 15:00:20 +0100

    add method hitung,prioritas,tampil,cekKali,aritmatika

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -8,11 +8,12 @@ namespace Calculator
 {
     class Calculator
     {
-        private string input { set; get; } //Declare string input for menampung masukkan
-        private ArrayList data = new ArrayList();
-        public Calculator(string inp)  //declare Constructore
+        private string inp { set; get; } //Declare string input for menampung masukkan
+        private ArrayList arr;
+        public Calculator(string inp,ArrayList arr)  //declare Constructore
         {
-            input = inp;
+            this.inp = inp;
+            this.arr = arr;
         }
         private bool validasi(string inp)
         {
@@ -40,32 +41,33 @@ namespace Calculator
                 }
             return cekTrue;
         }
-        public void convert(string inp, ArrayList arr)
+        public void convert()
         {
-            if (validasi(input)) //cek validasi TRUE atau False
+            if (validasi(inp)) //cek validasi TRUE atau False
             {
-                string tmp="";  //untuk tempat sementara bilangan
+                string tmp = "";  //untuk tempat sementara bilangan
                 if (inp[0] != '(')
                     tmp += inp[0];
                 else
                     arr.Add(inp[0]);
                 for (int i = 1; i < inp.Length; i++) //for dari inp[1] bukan inp[0]
                 {
-                    if(inp[i]=='^'||inp[i]=='*'||inp[i]=='/'||inp[i]=='+'||inp[i]=='('||inp[i]==')')
+                    if (inp[i] == '^' || inp[i] == '*' || inp[i] == '/' || inp[i] == '+' || inp[i] == '(' || inp[i] == ')')
                     {
-                        arr.Add(Convert.ToDouble(tmp));
+                        if(tmp!="")
+                            arr.Add(Convert.ToDouble(tmp));
                         tmp = "";
                         arr.Add(inp[i]);
                     }
-                    else if(inp[i]=='-')
+                    else if (inp[i] == '-')
                     {
-                        if(inp[i-1]=='^'||inp[i-1]=='*'||inp[i-1]=='/'||inp[i-1]=='+')
-                            tmp+=inp[i];
+                        if (inp[i - 1] == '^' || inp[i - 1] == '*' || inp[i - 1] == '/' || inp[i - 1] == '+')
+                            tmp += inp[i];
                         else
                             arr.Add(inp[i]);
                     }
                     else
-                        tmp+=inp[i];
+                        tmp += inp[i];
                 }
                 if (tmp != "")
                     arr.Add(Convert.ToDouble(tmp));
@@ -73,6 +75,103 @@ namespace Calculator
             else
                 Console.WriteLine("Error!!!...");
         }
-
+        private double hitung(double op1, double op2, char op) //Fungsi Perhitungan ^,*,/,+,-
+        {
+            if (op == '^')
+            {
+                double tmp=op1;
+                for (int i = 2; i <= op2;i++)
+                    tmp *= op1;
+                return tmp;
+            }
+            else if(op=='*')
+                return op1*op2;
+            else if(op=='/')
+                return op1/op2;
+            else if(op=='+')
+                return op1+op2;
+            else
+                return op1-op2;
+        }
+        private int prioritas(int start, int end) //Mengambil posisi Prioritas operator 
+        {
+            if (arr.IndexOf('^', start, end - start) != -1)
+                return arr.IndexOf('^', start, end - start);
+            else if(arr.IndexOf('*', start, end - start) != -1)
+                return arr.IndexOf('*', start, end - start);
+            else if (arr.IndexOf('/', start, end - start) != -1)
+                return arr.IndexOf('/', start, end - start);
+            else if (arr.IndexOf('+', start, end - start) != -1)
+                return arr.IndexOf('+', start, end - start);
+            else
+                return arr.IndexOf('-', start, end - start);
+        }
+        public void tampil()
+        {
+            for (int i = 0; i < arr.Count; i++)
+                Console.Write("{0}", arr[i]);
+            Console.WriteLine();
+        }
+        private void cekKali()  //Untuk mengecek input * mis 2(2-2), akan diubah menjadi 2*(2-2)
+        {
+            int tmp = arr.IndexOf('(');
+            while (tmp != -1)
+            {
+                if ((char)arr[tmp - 1] != '(' && (char)arr[tmp - 1] != '^' && (char)arr[tmp - 1] != '*' && (char)arr[tmp - 1] != '/' && (char)arr[tmp - 1] != '+' && (char)arr[tmp - 1] != '-')
+                {
+                    arr.Insert(tmp, '*');
+                    tmp++;
+                }
+                tmp = arr.IndexOf('(', tmp + 1);
+            }
+            tmp = arr.LastIndexOf(')', arr.Count - 2);
+            while (tmp != -1)
+            {
+                if ((char)arr[tmp +1] != ')' && (char)arr[tmp + 1] != '^' && (char)arr[tmp + 1] != '*' && (char)arr[tmp + 1] != '/' && (char)arr[tmp + 1] != '+' && (char)arr[tmp + 1] != '-')
+                {
+                    arr.Insert(tmp+1, '*');
+                    tmp--;
+                }
+                tmp = arr.LastIndexOf(')', tmp - 1);
+            }
+        }
+        public void aritmatika()
+        {
+            cekKali();
+            bool cekKurung;
+            if (arr.IndexOf('(') != -1)
+                cekKurung = true;
+            else
+                cekKurung = false;
+            while (cekKurung == true)
+            {
+                int bKurung = arr.LastIndexOf('(');
+                int tKurung = arr.IndexOf(')', bKurung);
+                int pos;
+                while (tKurung - bKurung > 2)
+                {
+                    pos = prioritas(bKurung + 1, tKurung - 1);
+                    arr[pos - 1] = hitung(Convert.ToDouble(arr[pos - 1]), Convert.ToDouble(arr[pos + 1]), (char)arr[pos]);
+                    arr.RemoveAt(pos);
+                    arr.RemoveAt(pos);
+                    tKurung = arr.IndexOf(')', bKurung);
+                    tampil();
+                }
+                arr.RemoveAt(tKurung);
+                arr.RemoveAt(bKurung);
+                if (arr.IndexOf('(') != -1)
+                    cekKurung = true;
+                else
+                    cekKurung = false;
+            }
+            while (arr.Count != 1)
+            {
+                int pos = prioritas(0, arr.Count);
+                arr[pos - 1] = hitung(Convert.ToDouble(arr[pos - 1]), Convert.ToDouble(arr[pos + 1]), (char)arr[pos]);
+                arr.RemoveAt(pos);
+                arr.RemoveAt(pos);
+                tampil();
+            }
+        }
     }
 }
