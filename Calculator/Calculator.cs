78a9bd6ca7c14bbae3d51487bc7e955c824d5c13
suryadi121 <suryadi.surya5@gmail.com>
2014-04-28 15:18:39 +0700

    version 1 rilis, fix bug

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -10,81 +10,121 @@ namespace Calculator
     {
         private string inp { set; get; } //Declare string input for menampung masukkan
         private ArrayList arr;
-        private bool cekError;  //True jika Tidak ada Error
+        public bool cekError=true;  //True jika Tidak ada Error
         public Calculator(string inp,ArrayList arr)  //declare Constructore
         {
             this.inp = inp;
             this.arr = arr;
             cekError = true;
         }
-        public bool validasi()
+        public void validasi()
         {
-            bool cekTrue = true;
-            if (inp == "")  //Validasi
+            if (inp == "" || inp.Length == 1)  //Validasi
             {
-                Console.WriteLine("Input False");
-                cekTrue = false;
+                cekError = false;
             }
             else  //Validasi
+            {
                 foreach (char i in inp)
                 {
                     if (!Char.IsDigit(i) && i != '^' && i != '*' && i != '/' && i != '+' && i != '-' && i != '(' && i != ')' && i != '.')
                     {
-                        Console.WriteLine("Input False");
-                        cekTrue = false;
+                        cekError = false;
                         break;
                     }
                 }
-            return cekTrue;
-        }
-        private bool validasiTITIK(string str)  //return TRUE jika tidak ada error
-        {
-            int cek=0;
-            foreach(char i in str)
-                if(i=='.')
-                    cek++;
-            if(cek<2)
-                return true;
-            else
-                return false;
-        }
-        public void convert()
-        {
-            string tmp = "";  //untuk tempat sementara bilangan
-            if (inp[0] != '(' && inp[0] != ')' && inp[0] == '^' && inp[0] == '*' && inp[0] == '/' && inp[0] == '+' && inp[0] == '-')
-                tmp += inp[0];
-            else
-                arr.Add(inp[0]);
-            for (int i = 1; i < inp.Length; i++) //for dari inp[1] bukan inp[0]
-            {
-                if (inp[i] == '^' || inp[i] == '*' || inp[i] == '/' || inp[i] == '+' || inp[i] == '(' || inp[i] == ')')
+                for (int i = 1; i < inp.Length; i++)
                 {
-                    if (tmp != "" && validasiTITIK(tmp))
-                        arr.Add(Convert.ToDouble(tmp));
-                    else if (!validasiTITIK(tmp))
+                    if (inp[i - 1] == '(' && inp[i] == ')')
                     {
                         cekError = false;
                         break;
                     }
-                    tmp = "";
-                    arr.Add(inp[i]);
+                    if (inp[i - 1] == ')' && inp[i] == '(')
+                    {
+                        cekError = false;
+                        break;
+                    }
                 }
-                else if (inp[i] == '-')
-                {
-                    if (inp[i - 1] == '^' || inp[i - 1] == '*' || inp[i - 1] == '/' || inp[i - 1] == '+')
-                        tmp += inp[i];
-                    else
-                        arr.Add(inp[i]);
-                }
-                else
-                    tmp += inp[i];
+                if (inp[0] == ')')
+                    cekError = false;
+                if (inp[inp.Length - 1] == '(')
+                    cekError = false;
             }
-            if (tmp != "" && validasiTITIK(tmp))
-                arr.Add(Convert.ToDouble(tmp));
-            else if (!validasiTITIK(tmp))
-                cekError = false;
-            if(cekError==false)
-                Console.WriteLine("Input False");
+        }
+        public void convert()
+        {
+            string tmp = "";  //untuk tempat sementara bilangan
+            if (inp[0] != '(' && inp[0] != ')' && inp[0] != '^' && inp[0] != '*' && inp[0] != '/' && inp[0] != '+')
+                tmp += inp[0];
+            else if (inp[0] == '(')
+                arr.Add(inp[0]);
+            else
+                cekError = true;
+            if (cekError == true)
+            {
+                for (int i = 1; i < inp.Length; i++) //for dari inp[1] bukan inp[0]
+                {
+                    if (inp[i] == '^' || inp[i] == '*' || inp[i] == '/' || inp[i] == '+' || inp[i] == '(' || inp[i] == ')')
+                    {
+                        try
+                        {
+                            arr.Add(Convert.ToDouble(tmp));
+                        }
+                        catch (Exception)
+                        {
+                            cekError = false;
+                            break;
+                        }
+                        tmp = "";
+                        if (arr[arr.Count - 1].ToString() != "(" && arr[arr.Count - 1].ToString() != "^" && arr[arr.Count - 1].ToString() != "*" && arr[arr.Count - 1].ToString() != "/" && arr[arr.Count - 1].ToString() != "+" && arr[arr.Count - 1].ToString() != "-")
+                            arr.Add(inp[i]);
+                        else
+                        {
+                            cekError = false;
+                            break;
+                        }
+                    }
+                    else if (inp[i] == '-')
+                    {
+                        if (inp[i - 1] == '^' || inp[i - 1] == '*' || inp[i - 1] == '/' || inp[i - 1] == '+')
+                            tmp += inp[i];
+                        else
+                        {
+                            try
+                            {
+                                arr.Add(Convert.ToDouble(tmp));
+                            }
+                            catch (Exception)
+                            {
+                                cekError = false;
+                                break;
+                            }
+                            tmp = "";
+                            if (arr[arr.Count - 1].ToString() != "(" && arr[arr.Count - 1].ToString() != "^" && arr[arr.Count - 1].ToString() != "*" && arr[arr.Count - 1].ToString() != "/" && arr[arr.Count - 1].ToString() != "+" && arr[arr.Count - 1].ToString() != "-")
+                                arr.Add(inp[i]);
+                            else
+                            {
+                                cekError = false;
+                                break;
+                            }
+                        }
+                    }
+                    else
+                        tmp += inp[i];
+                }
+                if (tmp != "")
+                {
+                    try
+                    {
+                        arr.Add(Convert.ToDouble(tmp));
+                    }
+                    catch (Exception)
+                    {
+                        cekError = false;
+                    }
+                }
+            }
         }
         private double hitung(double op1, double op2, char op) //Fungsi Perhitungan ^,*,/,+,-
         {
@@ -119,30 +159,33 @@ namespace Calculator
         }
         private void tampil()
         {
+            Console.Write("\t ::> ");
             for (int i = 0; i < arr.Count; i++)
-                Console.Write("{0}", arr[i]);
+            {
+                if (arr[i].ToString() == "^" || arr[i].ToString() == "*" || arr[i].ToString() == "/" || arr[i].ToString() == "+" || arr[i].ToString() == "-")
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(" {0} ", arr[i]);
+                }
+                else if(arr[i].ToString() == "(" || arr[i].ToString() == ")")
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write("{0}", arr[i]);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write("{0}", arr[i]);
+                }
+            }
+            Console.ResetColor();
             Console.WriteLine();
         }
         private void cekKali()  //Untuk mengecek input * mis 2(2-2), akan diubah menjadi 2*(2-2)
         {
-            for (int i = 1; i < arr.Count; i++)
+            if (arr.IndexOf('(') != -1)     //Akan dilakukan pengecekan jika ada input '('
             {
-                if (arr[i - 1].ToString() == "(" && arr[i].ToString() == ")")
-                {
-                    cekError = false;
-                    Console.WriteLine("Input False");
-                    break;
-                }
-                else if (arr[i - 1].ToString() == ")" && arr[i].ToString() == "(")
-                {
-                    cekError = false;
-                    Console.WriteLine("Input False");
-                    break;
-                }
-            }
-            if (cekError)
-            {
-                int tmp = arr.IndexOf('(');
+                int tmp = arr.IndexOf('(',1);
                 while (tmp != -1)
                 {
                     if (arr[tmp - 1].ToString() != "(" && arr[tmp - 1].ToString() != "^" && arr[tmp - 1].ToString() != "*" && arr[tmp - 1].ToString() != "/" && arr[tmp - 1].ToString() != "+" && arr[tmp - 1].ToString() != "-")
@@ -155,7 +198,7 @@ namespace Calculator
                 tmp = arr.LastIndexOf(')', arr.Count - 2);
                 while (tmp != -1)
                 {
-                    if (arr[tmp + 1].ToString() != ")" && arr[tmp + 1].ToString() != "^" && arr[tmp + 1].ToString() != "*" && arr[tmp + 1].ToString() != "/" && arr[tmp + 1].ToString() != "+" && arr[tmp + 1].ToString() != "-")
+                    if (arr[tmp + 1].ToString() != "(" && arr[tmp + 1].ToString() != ")" && arr[tmp + 1].ToString() != "^" && arr[tmp + 1].ToString() != "*" && arr[tmp + 1].ToString() != "/" && arr[tmp + 1].ToString() != "+" && arr[tmp + 1].ToString() != "-")
                     {
                         arr.Insert(tmp + 1, '*');
                         tmp--;
@@ -166,74 +209,81 @@ namespace Calculator
         }
         private void validasiOperator()
         {
-            if (cekError == true)
+            int counterKurung = 0;
+            int counterOperator = 0;
+            for (int i = 0; i < arr.Count; i++)
             {
-                int counterKurung = 0;
-                int counterOperator = 0;
-                for (int i = 0; i < arr.Count; i++)
-                {
-                    if (counterKurung < 0)
-                        break;
-                    if (arr[i].ToString() == "(")
-                        counterKurung++;
-                    else if (arr[i].ToString() == ")")
-                        counterKurung--;
-                    else if (arr[i].ToString() == "^" || arr[i].ToString() == "*" || arr[i].ToString() == "/" || arr[i].ToString() == "+" || arr[i].ToString() == "-")
-                        counterOperator--;
-                    else
-                        counterOperator++;
-                }
-                if (counterKurung == 0 && counterOperator == 1)
-                    cekError = true;
+                if (counterKurung < 0)
+                    break;
+                if (arr[i].ToString() == "(")
+                    counterKurung++;
+                else if (arr[i].ToString() == ")")
+                    counterKurung--;
+                else if (arr[i].ToString() == "^" || arr[i].ToString() == "*" || arr[i].ToString() == "/" || arr[i].ToString() == "+" || arr[i].ToString() == "-")
+                    counterOperator--;
                 else
-                {
-                    cekError = false;
-                    Console.WriteLine("Input False");
-                }
+                    counterOperator++;
             }
+            if (counterKurung != 0 || counterOperator != 1)
+                cekError = false;
         }
         public void aritmatika()
         {
             cekKali();
-            validasiOperator();
-            if (cekError)
+            if (arr.Count <= 2)
             {
-                tampil();
-                cekKali();
-                bool cekKurung;
+                cekError = false;
+                Console.WriteLine("Input False");
+            }
+            bool cekKurung=false;
+            if (arr.IndexOf('(') != -1)
+                cekKurung = true;
+            while (cekKurung == true && cekError==true)
+            {
+                int bKurung = arr.LastIndexOf('(');
+                int tKurung = arr.IndexOf(')', bKurung);
+                int pos;
+                while (tKurung - bKurung > 2)
+                {
+                    pos = prioritas(bKurung + 1, tKurung - 1);
+                    try
+                    {
+                        arr[pos - 1] = hitung(Convert.ToDouble(arr[pos - 1]), Convert.ToDouble(arr[pos + 1]), (char)arr[pos]);
+                    }
+                    catch (Exception)
+                    {
+                        cekError = false;
+                        Console.WriteLine("Input False");
+                        break;
+                    }
+                    arr.RemoveAt(pos);
+                    arr.RemoveAt(pos);
+                    tKurung = arr.IndexOf(')', bKurung);
+                    tampil();
+                }
+                arr.RemoveAt(tKurung);
+                arr.RemoveAt(bKurung);
                 if (arr.IndexOf('(') != -1)
                     cekKurung = true;
                 else
                     cekKurung = false;
-                while (cekKurung == true)
+            }
+            while (arr.Count != 1 && cekError==true)
+            {
+                int pos = prioritas(0, arr.Count);
+                try
                 {
-                    int bKurung = arr.LastIndexOf('(');
-                    int tKurung = arr.IndexOf(')', bKurung);
-                    int pos;
-                    while (tKurung - bKurung > 2)
-                    {
-                        pos = prioritas(bKurung + 1, tKurung - 1);
-                        arr[pos - 1] = hitung(Convert.ToDouble(arr[pos - 1]), Convert.ToDouble(arr[pos + 1]), (char)arr[pos]);
-                        arr.RemoveAt(pos);
-                        arr.RemoveAt(pos);
-                        tKurung = arr.IndexOf(')', bKurung);
-                        tampil();
-                    }
-                    arr.RemoveAt(tKurung);
-                    arr.RemoveAt(bKurung);
-                    if (arr.IndexOf('(') != -1)
-                        cekKurung = true;
-                    else
-                        cekKurung = false;
-                }
-                while (arr.Count != 1)
-                {
-                    int pos = prioritas(0, arr.Count);
                     arr[pos - 1] = hitung(Convert.ToDouble(arr[pos - 1]), Convert.ToDouble(arr[pos + 1]), (char)arr[pos]);
-                    arr.RemoveAt(pos);
-                    arr.RemoveAt(pos);
-                    tampil();
                 }
+                catch
+                {
+                    cekError = false;
+                    Console.WriteLine("Input False");
+                    break;
+                }
+                arr.RemoveAt(pos);
+                arr.RemoveAt(pos);
+                tampil();
             }
         }
     }
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace Calculator
 {
@@ -10,6 +11,37 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Calculator calc;
+            string input;
+            do
+            {
+                Console.Clear();
+                ArrayList data = new ArrayList();
+                Console.Write("Hitung   ::> ");
+                input = Console.ReadLine();
+                if (input == "0")
+                {
+                    Console.SetCursorPosition(20, 20);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("Thanks for using this Calculator...");
+                }
+                else
+                {
+                    calc = new Calculator(input, data);
+                    calc.validasi();
+                    if (calc.cekError == true)
+                    {
+                        calc.convert();
+                        if (calc.cekError == true)
+                            calc.aritmatika();
+                        else
+                            Console.WriteLine("Input False");
+                    }
+                    else
+                        Console.WriteLine("Input False");
+                }
+                Console.ReadKey();
+            } while (input != "0");
         }
     }
 }
